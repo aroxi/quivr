268c952291518374e5008f23499cd69abe4837a4
Matteo <59047004+aroxi@users.noreply.github.com>
2023-07-08 12:06:01 +0200

    working don't touch footer

--- frontend/lib/components/Footer/index.tsx
+++ frontend/lib/components/Footer/index.tsx
@@ -12,7 +12,34 @@ const Footer = (): JSX.Element => {
   return (
     <footer className="bg-white dark:bg-black border-t dark:border-white/10 mt-auto py-4">
       <div className="max-w-screen-xl mx-auto flex justify-center items-center gap-4">
-        Merzost Experimental 0.43 Beta by Arox
+        <a
+          href={GITHUB_URL}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Quivr GitHub"
+        >
+          <img
+            className="h-8 w-auto dark:invert"
+            src="/github.svg"
+            alt="GitHub"
+          />
+        </a>
+        <a
+          href={TWITTER_URL}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Quivr Twitter"
+        >
+          <img className="h-8 w-auto" src="/twitter.svg" alt="Twitter" />
+        </a>
+        <a
+          href={DISCORD_URL}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Quivr Discord"
+        >
+          <img className="h-8 w-auto" src="/discord.svg" alt="Discord" />
+        </a>
       </div>
     </footer>
   );
